3487cd821c4f6484ea09f8cd2927a837fb3ac0f9
leocodeio <basicleo@787>
2024-08-20 20:59:38 +0530

    all ok?

--- client/app.js
+++ client/app.js
@@ -1,64 +1,56 @@
-
+const apiUrl = "http://127.0.0.1:5000";
 
 function getBathValue() {
     var uiBathrooms = document.getElementsByName("uiBathrooms");
-    for(var i in uiBathrooms) {
-      if(uiBathrooms[i].checked) {
-          return parseInt(i)+1;
-      }
+    for (var i in uiBathrooms) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
+        }
     }
     return -1; // Invalid Value
-  }
-  
-  function getBHKValue() {
+}
+
+function getBHKValue() {
     var uiBHK = document.getElementsByName("uiBHK");
-    for(var i in uiBHK) {
-      if(uiBHK[i].checked) {
-          return parseInt(i)+1;
-      }
+    for (var i in uiBHK) {
+        if (uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
     }
     return -1; // Invalid Value
-  }
-  
-  function onClickedEstimatePrice() {
+}
+
+function onClickedEstimatePrice() {
     console.log("Estimate price button clicked");
-    var sqft = document.getElementById("uiSqft");
+    var sqft = document.getElementById("uiSqft").value;
     var bhk = getBHKValue();
     var bathrooms = getBathValue();
-    var location = document.getElementById("uiLocations");
+    var location = document.getElementById("uiLocations").value;
     var estPrice = document.getElementById("uiEstimatedPrice");
-  
-    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  
-    $.post(url, {
-        total_sqft: parseFloat(sqft.value),
+
+    $.post(`${apiUrl}/predict_home_price`, {
+        total_sqft: parseFloat(sqft),
         bhk: bhk,
         bath: bathrooms,
-        location: location.value
-    },function(data, status) {
+        location: location
+    }, function(data, status) {
         console.log(data.estimated_price);
         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
         console.log(status);
     });
-  }
-  
-function onPageLoad() {
-console.log( "document loaded" );
-var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-// var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-$.get(url,function(data, status) {
-    console.log("got response for get_location_names request");
-    console.log(data)
-    var locations = data.location;
-    var uiLocations = document.getElementById("uiLocations");
-    $('#uiLocations').empty();
-    for(var i in locations) {
-        var opt = new Option(locations[i]);
-        $('#uiLocations').append(opt);
-    }
-    
-});
 }
 
-window.onload = onPageLoad; 
+function onPageLoad() {
+    console.log("document loaded");
+    $.get(`${apiUrl}/get_location_names`, function(data, status) {
+        console.log("got response for get_location_names request");
+        var locations = data.location;
+        $('#uiLocations').empty();
+        for (var i in locations) {
+            var opt = new Option(locations[i]);
+            $('#uiLocations').append(opt);
+        }
+    });
+}
+
+window.onload = onPageLoad;
